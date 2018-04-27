39d89202095b120ff7d9a565472008647b427557
Jacek Olearczyk <rurku@rurku.com>
2018-04-27 22:02:29 +0200

    Authenticate github api request to get release id
    This should fix the error about api requests limit per ip

--- tools/after_deploy.ps1
+++ tools/after_deploy.ps1
@@ -3,12 +3,15 @@ if ($env:TRAVIS_TAG -NotLike '*-*')
 {
   "It's not a prerelease tag. Mark release as not prerelease."
   "Getting release ID for tag $($env:TRAVIS_TAG)"
-  $response = Invoke-WebRequest -Uri "https://api.github.com/repos/$($env:TRAVIS_REPO_SLUG)/releases/tags/$($env:TRAVIS_TAG)" | ConvertFrom-Json
+  $response = Invoke-WebRequest `
+    -Uri "https://api.github.com/repos/$($env:TRAVIS_REPO_SLUG)/releases/tags/$($env:TRAVIS_TAG)"  `
+    -Headers @{Authorization="token $($env:GITHUB_RELEASE_TOKEN)"} ` 
+    | ConvertFrom-Json
   "Setting release as not prerelease"
   $patchResponse = Invoke-WebRequest `
-	-Uri "https://api.github.com/repos/$($env:TRAVIS_REPO_SLUG)/releases/$($response.id)" `
-	-Method PATCH -ContentType 'application/json' `
-	-Headers @{Authorization="token $($env:GITHUB_RELEASE_TOKEN)"} `
-	-Body '{"prerelease": false}'
+    -Uri "https://api.github.com/repos/$($env:TRAVIS_REPO_SLUG)/releases/$($response.id)" `
+    -Method PATCH -ContentType 'application/json' `
+    -Headers @{Authorization="token $($env:GITHUB_RELEASE_TOKEN)"} `
+    -Body '{"prerelease": false}'
   "Status code: $($patchResponse.StatusCode)"
 }
