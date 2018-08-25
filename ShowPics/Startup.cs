cd0b0d67877a51ad6f8771320a1937fd045b15ac
Jacek Olearczyk <rurku@rurku.com>
2018-08-25 14:13:06 +0200

    fix dependencies

--- ShowPics/Startup.cs
+++ ShowPics/Startup.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using ShowPics.Utilities.Settings;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 
 namespace ShowPics
 {
@@ -40,10 +40,6 @@ namespace ShowPics
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-                {
-                    HotModuleReplacement = true
-                });
             }
             else
             {
